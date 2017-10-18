0ba2e6e39d96924b9fe06dc4a334e808fcbe1c5c
pali192 <pali.gunar@gmail.com>
2017-10-18 18:04:04 +0200

    Cviko2

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -11,7 +11,7 @@ extern const char __STABSTR_BEGIN__[];		// Beginning of string table
 extern const char __STABSTR_END__[];		// End of string table
 
 
-// stab_binsearch(stabs, region_left, region_right, type, addr)
+//stab_binsearch(stabs, region_left, region_right, type, addr)
 //
 //	Some stab types are arranged in increasing order by instruction
 //	address.  For example, N_FUN stabs (stab entries with n_type ==
@@ -47,6 +47,7 @@ extern const char __STABSTR_END__[];		// End of string table
 //		stab_binsearch(stabs, &left, &right, N_SO, 0xf0100184);
 //	will exit setting left = 118, right = 554.
 //
+
 static void
 stab_binsearch(const struct Stab *stabs, int *region_left, int *region_right,
 	       int type, uintptr_t addr)
@@ -179,6 +180,8 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	//	Look at the STABS documentation and <inc/stab.h> to find
 	//	which one.
 	// Your code here.
+	stab_binsearch(stabs, &lline, &rline, N_SLINE, addr);
+	info->eip_line = stabs[lline].n_desc;
 
 
 	// Search backwards from the line number for the relevant filename
